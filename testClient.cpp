0ef9ca0251e8a24a20656a268065d4a892e67b63
Tyeia <Tyeia@users.noreply.github.com>
2020-05-05 16:57:58 -0400

    Trying things

--- testClient.cpp
+++ testClient.cpp
@@ -8,19 +8,21 @@ using namespace boost::beast::websocket;
 
 int main(int argc, char const *argv[]) {
   net::io_context ioc;
+  std::string host = "71.213.21.10";
   stream<net::ip::tcp::socket> ws(ioc);
   net::ip::tcp::resolver resolver(ioc);
-  auto const resolved = resolver.resolve("192.168.0.222", "80");
+  auto const resolved = resolver.resolve(host, "80");
   // Connect the socket to the IP address returned from performing a name lookup
   net::connect(ws.next_layer(), resolved.begin(), resolved.end());
 
-  ws.handshake("192.168.0.100", "/");
+  ws.handshake(host, "/");
   std::cout << "Connected." << '\n';
-  while(true)
+  std::string msg;
+  while(msg != "!quit~")
   {
-    std::string msg;
     std::cin >> msg;
     ws.write(net::buffer(msg));
   }
+  ws.close(close_code::normal);
   return 0;
 }
--- testServer.cpp
+++ testServer.cpp
@@ -1,15 +1,18 @@
 #include <boost/beast.hpp>
 #include <boost/asio.hpp>
 #include <iostream>
+#include <pthread.h>
+#include <vector>
 namespace net = boost::asio;
 namespace beast = boost::beast;
 using namespace boost::beast;
 using namespace boost::beast::websocket;
 
 int main(int argc, char const *argv[]) {
+  std::vector<pthread_t> clients;
   net::io_context ioc;
   net::ip::tcp::acceptor acceptor(ioc);
-  net::ip::tcp::endpoint endpoint(net::ip::tcp::v4(), 1337);
+  net::ip::tcp::endpoint endpoint(net::ip::tcp::v4(), 80);
   acceptor.open(endpoint.protocol());
   acceptor.bind(endpoint);
   std::cout << "Endpoint Bound." << '\n';
@@ -22,13 +25,23 @@ int main(int argc, char const *argv[]) {
   // Perform the websocket handshake in the server role.
   // The stream must already be connected to the peer.
 
-  ws.accept();
-  std::cout << "Handshake complete." << '\n';
+  while(true)
+  {
+    ws.accept();
+    std::cout << "Handshake complete." << '\n';
+    clients.push_back();
+    pthread_create(&clients.back(),NULL,&clientHandler,NULL);
+  }
+  return 0;
+}
+
+void* clientHandler (void*)
+{
   while(true)
   {
     multi_buffer buffer;
     ws.read(buffer);
-    std::cout << buffers_to_string(buffer.data()) << '\n';
+    std::string msg = buffers_to_string(buffer.data());
+    std::cout << msg << '\n';
   }
-  return 0;
 }
