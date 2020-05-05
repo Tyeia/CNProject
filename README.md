2d2ae4f3de8db1fba62096d0924b8a242f2f28f4
tyeia <tyeia6633@gmail.com>
2020-05-05 15:25:39 +0000

    stuff

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 Project for Computer Networks Spring '20
 
 Messaging App with encryption
+
+Tyler Moff
+Romko Smuk
+Aaron Tobias
---
+++ main.cpp
@@ -0,0 +1,11 @@
+//this is a test file! rename before submission
+//-Aaron
+//I renamed it -Tyler
+//noice
+
+int main()
+{
+	//do stuff here
+	//we should probably add stuff... 
+	//it's due in like a week
+}
