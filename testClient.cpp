dbeea9638bd2821ce0e76941011702cbe2dbe985
Tyeia <Tyeia@users.noreply.github.com>
2020-05-05 23:25:25 -0400

    Connection Working, Message Handling not

--- testClient.cpp
+++ testClient.cpp
@@ -7,24 +7,34 @@ using namespace boost::beast;
 using namespace boost::beast::websocket;
 
 int main(int argc, char const *argv[]) {
-  net::io_context ioc;
-  std::string host = "localhost";
-  stream<net::ip::tcp::socket> ws(ioc);
-  net::ip::tcp::resolver resolver(ioc);
-  auto const resolved = resolver.resolve(host, "80");
-  // Connect the socket to the IP address returned from performing a name lookup
-  net::connect(ws.next_layer(), resolved.begin(), resolved.end());
 
-  ws.handshake(host, "/");
-  std::cout << "Connected." << '\n';
-  std::string msg;
-  ws.text(true);
-  while(msg != "!quit~\n")
+  try
   {
-    std::cin >> msg;
-    msg += "\n";
-    ws.write(net::buffer(msg));
+    net::io_context ioc;
+    std::string host = "localhost";
+    stream<net::ip::tcp::socket> ws(ioc);
+    net::ip::tcp::resolver resolver(ioc);
+    auto const resolved = resolver.resolve(host, "80");
+    // Connect the socket to the IP address returned from performing a name lookup
+    net::connect(ws.next_layer(), resolved.begin(), resolved.end());
+
+    ws.handshake(host, "/");
+    std::cout << "Connected." << '\n';
+    std::string msg;
+    ws.text(true);
+    while(msg != "!quit~")
+    {
+      std::cin >> msg;
+      ws.write(net::buffer(msg));
+    }
+    ws.close(close_code::normal);
   }
-  ws.close(close_code::normal);
+  catch(error_code ec)
+  {
+    if(ec == websocket::error::closed)
+    {
+      return 0;
+    }
+  }
   return 0;
 }
--- testServer.cpp
+++ testServer.cpp
@@ -7,44 +7,51 @@ using namespace boost::beast;
 using namespace boost::beast::websocket;
 
 int main(int argc, char const *argv[]) {
-  net::io_context ioc;
-  net::ip::tcp::acceptor acceptor(ioc);
-  net::ip::tcp::endpoint endpoint(net::ip::tcp::v4(), 80);
-  acceptor.open(endpoint.protocol());
-  acceptor.bind(endpoint);
-  std::cout << "Endpoint Bound." << '\n';
-  while(true)
+  try
   {
-    acceptor.listen();
-    std::cout << "Listening..." << '\n';
-    // The socket returned by accept() will be forwarded to the tcp_stream,
-    // which uses it to perform a move-construction from the net::ip::tcp::socket.
-
-    stream<net::ip::tcp::socket> ws(acceptor.accept());
-    // Perform the websocket handshake in the server role.
-    // The stream must already be connected to the peer.
-
-    ws.accept();
-    std::cout << "Handshake complete." << '\n';
-    multi_buffer buffer;
-    while(buffers_to_string(buffer.data())!= "!quit~")
+    net::io_context ioc;
+    net::ip::tcp::acceptor acceptor(ioc);
+    net::ip::tcp::endpoint endpoint(net::ip::tcp::v4(), 80);
+    acceptor.open(endpoint.protocol());
+    acceptor.bind(endpoint);
+    std::cout << "Endpoint Bound." << '\n';
+    while(true)
     {
+      acceptor.listen();
+      std::cout << "Listening..." << '\n';
+      // The socket returned by accept() will be forwarded to the tcp_stream,
+      // which uses it to perform a move-construction from the net::ip::tcp::socket.
 
-      char compare = '0';
-      std::string msg;
+      stream<net::ip::tcp::socket> ws(acceptor.accept());
+      // Perform the websocket handshake in the server role.
+      // The stream must already be connected to the peer.
 
-      while(compare != '\n')
+      ws.accept();
+      std::cout << "Handshake complete." << '\n';
+      multi_buffer buffer;
+      while(buffers_to_string(buffer.data())!= "!quit~")
       {
+        std::string msg;
         multi_buffer reset;
         buffer=reset;
-        compare = buffers_to_string(buffer.data()).back();
         ws.read(buffer);
         ws.text(ws.got_text());
         msg = buffers_to_string(buffer.data());
-        msg.erase(std::remove(msg.begin(), msg.end(), '\n'), msg.end());
-        std::cout << msg << ' ';
+        while(!ws.is_message_done())
+        {
+          std::cout << msg << ' ';
+          ws.read(buffer);
+          msg = buffers_to_string(buffer.data());
+        }
+        std::cout << msg << "\n";
       }
-      std::cout << msg<< '\n';
+    }
+  }
+  catch(error_code ec)
+  {
+    if(ec == net::error::operation_aborted||ec==websocket::error::closed)
+    {
+      return 0;
     }
   }
   return 0;
